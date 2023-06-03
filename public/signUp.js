16643bdbc29bfa7f05a1a8e4d0b749d0827d9bb2
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-06-03 19:23:58 +0530

    Update signUp.js

--- public/signUp.js
+++ public/signUp.js
@@ -6,10 +6,10 @@ const signUp = (e) => {
   const userDetails = { name, email, password };
 
   axios
-    .post("http://localhost:3000/user/add-user", userDetails)
+    .post("http://34.229.93.168:3000/user/add-user", userDetails)
     .then((res) => {
       window.alert("you are registered successfully!");
-      window.location.href = "http://127.0.0.1:5500/public/logIn.html";
+      window.location.href = "http://34.229.93.168:3000/public/logIn.html";
     })
     .catch((err) => console.log(err));
 };

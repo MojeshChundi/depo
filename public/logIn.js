986f1b1ad24aed43893cc58b14b56c29f5dc217a
MojeshChundi <112922928+MojeshChundi@users.noreply.github.com>
2023-06-03 19:25:55 +0530

    Update logIn.js

--- public/logIn.js
+++ public/logIn.js
@@ -8,13 +8,13 @@ function logIns(e) {
   const userDetails = { email, password };
   //console.log(userDetails);
   axios
-    .post("http://localhost:3000/user/login", userDetails)
+    .post("http://34.229.93.168:3000/user/login", userDetails)
     .then((res) => {
       console.log(res.data.user[0].ispremium);
       localStorage.setItem("token", res.data.token);
       localStorage.setItem("ispremium", res.data.user[0].ispremium);
       window.alert("login successfully!!");
-      window.location.href = "http://127.0.0.1:5500/public/expense.html";
+      window.location.href = "http://34.229.93.168:3000/public/expense.html";
     })
     .catch((err) => {
       const OutputHTML = `<ul style="list-style-type: none; background-color: #f8d7da; color: #721c24; padding: 10px; border: 2px solid #f5c6cb; border-radius: 5px; box-shadow: 0 0 10px rgba(0,0,0,.2); text-align: center;"><li>please enter valid username ans password</li></ul>`;
@@ -26,5 +26,5 @@ function logIns(e) {
 // forgot mail form
 
 function forgotMail() {
-  window.location.href = "http://127.0.0.1:5500/public/forgotMail.html";
+  window.location.href = "http://34.229.93.168:3000/public/forgotMail.html";
 }
